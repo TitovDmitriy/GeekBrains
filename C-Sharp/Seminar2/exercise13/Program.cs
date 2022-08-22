e1f90c0ec488051c60940456bbed91b51d91d12f
TitovDmitriy <titov-dp@yandex.ru>
2022-08-23 01:09:13 +0300

    HomeWork for Seminar2

---
+++ C-Sharp/Seminar2/exercise13/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random().Next(0, 100000);
+int num1 = num;
+if(num > 99 && num < 1000)
+{
+    Console.WriteLine(num1 + " " + (num % 100)  % 10);
+}
+if(num > 1000)
+{
+    Console.WriteLine(num1 + " " + (num % 1000)  / 100);
+}
+else
+{
+    Console.WriteLine(num1 + " третьей цифры нет");
+}
+
---
+++ C-Sharp/Seminar2/exercise15/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = new Random().Next(1, 8);
+if(num < 6)
+{
+    Console.WriteLine(num + " нет");
+}
+else
+{
+    Console.WriteLine(num + " да");
+}
+
