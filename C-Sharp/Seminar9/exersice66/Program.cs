21bd6966bacad9e7259fb6a82de7e7de404c4a19
TitovDmitriy <titov-dp@yandex.ru>
2022-09-16 17:46:00 +0300

    H/W for Seminar9

---
+++ C-Sharp/Seminar9/exersice66/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M = {M}; N = {N} -> {NaturalSum(M, N)}");
+
+int NaturalSum(int M, int N)
+{
+    if (M == N)
+        return N;
+    return M + NaturalSum(M + 1, N);
+}
