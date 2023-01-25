3a5285d8819915f5f010aa618e83df18cb221682
TitovDmitriy <titov-dp@yandex.ru>
2023-01-25 21:11:43 +0300

    HW3

---
+++ JAVA and basic API/Seminar3_JAVA_Collections_An_Introduction/HW3.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+// 1. Объявить два списка список ArrayList, в каждый добавить по 20 случайных чисел. 
+// Удалить из первого списка элементы отсутствующие во втором списке. Отсортировать 
+// первый список методом sort класса Collections.
+// 2. *Отсортировать второй список методом sort списка и компаратором по уменьшению.
+// 3. **Отсортировать первый список пузырьковой сортировкой самостоятельно, без использования
+//  доп методов и классов.
+
+
+public class HW3 {
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> lst1 = new ArrayList<>();
+        Random rnd = new Random();
+        int size = 20;
+        for (int i = 0; i < size; i++) {
+            int value = rnd.nextInt(50);
+            lst1.add(value);           
+        }
+
+        ArrayList<Integer> lst2 = new ArrayList<>();
+        rnd = new Random();
+        size = 20;
+        for (int i = 0; i < size; i++) {
+            int value = rnd.nextInt(50);
+            lst2.add(value);           
+        }
+        System.out.println(lst1);
+        System.out.println(lst2);
+        
+        ArrayList<Integer> templst1 = new ArrayList<>(lst1);
+
+
+        templst1.removeAll(lst2);   
+        System.out.println(templst1);    
+        lst1.removeAll(templst1); // Удаляем из первого списка элементы, которые отсутствуют во втором списке.        
+        Collections.sort(lst1); // Сортируем список по возростанию
+        System.out.println(lst1);
+        Collections.reverse(lst1); // Сортируем по уменьшению
+        System.out.println(lst1);       
+    }
+    
+}
